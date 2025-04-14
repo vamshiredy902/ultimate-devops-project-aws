b707befb87ed622a5ae4cc1a358285646a1b82f1
Vamsi <devopsaws925@gmail.com>
2025-04-14 10:37:34 +0530

    Update main.tf

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-buckert-s3-72662
+    bucket         = "demo-terraform-eks-state-buckert-s3-72662"
     key            = "terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-eks-state-locking-system"
