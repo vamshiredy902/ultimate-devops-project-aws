ee54e4ac11fc11e36da54e8186a6a7687e94eb82
Vamsi <devopsaws925@gmail.com>
2025-04-14 10:10:59 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket"
+  bucket = "demo-terraform-eks-state-buckert-s3-72662"
 
   lifecycle {
     prevent_destroy = false
